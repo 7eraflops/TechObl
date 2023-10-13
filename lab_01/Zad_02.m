abd603aab8b1d9686634f6961ce094df330a1605
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2023-10-13 15:15:07 +0200

    Zad_02 completed

---
+++ lab_01/Zad_02.m
@@ -0,0 +1,9 @@
+x=-100:0.01:100;
+coefficients=[-2,3,6,-5];
+y=polyval(coefficients,x);
+figure(1)
+    plot(x,y,"k",LineWidth=2)
+    grid on
+    title("y=-2x^3+3x^2+6x-5")
+    xlabel("x")
+    ylabel("y")
