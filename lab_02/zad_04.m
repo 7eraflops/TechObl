ed5fdf9c95c08bcb4078b673f606b1be50cdf4df
Michał Charaszkiewicz <michal.charaszkiewicz@gmail.com>
2023-10-27 00:59:50 +0200

    fixed formatting

--- lab_02/zad_04.m
+++ lab_02/zad_04.m
@@ -1,31 +1,30 @@
-a=-1;
-b=2;
-c=3;
-coefficients=[a,b,c];
-x=linspace(-5,5,2000);
-delta=b^2-4*a*c
+a = -1;
+b = 2;
+c = 3;
+coefficients = [a, b, c];
+x = linspace(-5, 5, 2000);
+delta = b ^ 2 - 4 * a * c
 
-x_1=(-1*b+sqrt(delta))/(2*a)
-x_2=(-1*b-sqrt(delta))/(2*a)
-y_1=a*x_1^2+b*x_1+c
-y_2=a*x_2^2+b*x_2+c
-zeros=[x_1,y_1;x_2,y_2];
+x_1 = (-1 * b + sqrt(delta)) / (2 * a)
+x_2 = (-1 * b - sqrt(delta)) / (2 * a)
+y_1 = a * x_1 ^ 2 + b * x_1 + c
+y_2 = a * x_2 ^ 2 + b * x_2 + c
+zeros = [x_1, y_1; x_2, y_2];
 
-y=a*x.^2+b*x+c;
+y = a * x .^ 2 + b * x + c;
 
-y_m=polyval(coefficients,x);
+y_m = polyval(coefficients, x);
 
-r=roots(coefficients);
+r = roots(coefficients);
 
 figure(1)
-plot(x,y,'y','LineWidth',2)
+plot(x, y, 'y', 'LineWidth', 2)
 hold on
-plot(zeros(:,1),zeros(:,2),'ro')
+plot(zeros(:, 1), zeros(:, 2), 'ro')
 
 grid on
 xlabel("x")
 ylabel("y")
 title("-x^2+2x+3")
 
-plot(x,y_m,'r--')
-
+plot(x, y_m, 'r--')
--- lab_02/zad_05.m
+++ lab_02/zad_05.m
@@ -1,16 +1,16 @@
-[X,Y]=meshgrid(-5:5);
-F_1=sin(X)+cos(Y);
-F_2=exp(-1*X.^2)+exp(-1*(Y.^2));
-F_3=X.^2+Y.^2;
+[X, Y] = meshgrid(-5:5);
+F_1 = sin(X) + cos(Y);
+F_2 = exp(-1 * X .^ 2) + exp(-1 * (Y .^ 2));
+F_3 = X .^ 2 + Y .^ 2;
 figure(1)
 
-subplot(3,1,1)
-surf(X,Y,F_1)
+subplot(3, 1, 1)
+surf(X, Y, F_1)
 title("sin(x)+cos(x)")
 
-subplot(3,1,2)
-surf(X,Y,F_2)
+subplot(3, 1, 2)
+surf(X, Y, F_2)
 title("e")
 
-subplot(3,1,3)
-surf(X,Y,F_3)
+subplot(3, 1, 3)
+surf(X, Y, F_3)
